5871fbb381e00f0bc67dbff801a3b016355a6e3a
David <davidoregan91@gmail.com>
2019-12-10 16:50:32 +0000

    feat(database): add database

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
+const product = require('./routes/Product'); 
 
-let port = 5000;
+const dev_db_url = 'mongodb://someuser:abcd1234@ds123619.mlab.com:23619/productstutorial';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/products', product);
+
+let port = 1234;
 
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
